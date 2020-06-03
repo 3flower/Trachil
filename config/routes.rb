d449e090a1f8331d59b71b4a395a20c81f22190d
atu <61017515+3flower@users.noreply.github.com>
2020-06-03 18:07:23 +0900

    [add]header

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     :sessions => 'users/sessions',
     :registrations => 'users/registrations'
    }
-   root 'homes#top'
-   get  'about'
+  root 'homes#top'
+  get  'about' => 'homes#about'
 end
