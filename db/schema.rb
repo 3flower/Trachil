4d1de3bbd5c2f3a96c7817427edf115087ed6de3
atu <61017515+3flower@users.noreply.github.com>
2020-06-03 17:07:40 +0900

    [add]devise

---
+++ db/schema.rb
@@ -0,0 +1,37 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_06_03_073032) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "residence"
+    t.text "introduction"
+    t.boolean "is_child", default: false, null: false
+    t.string "child_people", default: "1"
+    t.integer "child_age", default: 0
+    t.boolean "is_diaper", default: false, null: false
+    t.boolean "is_baby_food", default: false, null: false
+    t.string "user_image_id", default: "no_image.jpg"
+    t.boolean "is_valid", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
