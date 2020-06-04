09a7e22a46234754c464b3fd6da6a77a18063f76
atu <61017515+3flower@users.noreply.github.com>
2020-06-04 13:35:31 +0900

    devise signin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,12 @@
 module ApplicationHelper
   # デバイスをトップに持ってくるために記述
   def resource_name
-   :user
+    :user
   end
 
   def resource
      @resource ||= User.new
   end
-
   def devise_mapping
      @devise_mapping ||= Devise.mappings[:user]
   end
