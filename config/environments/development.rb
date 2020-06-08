2ca4d0ae71211b8804a67e48305714baf793f4dc
atu <61017515+3flower@users.noreply.github.com>
2020-06-08 16:19:23 +0900

    [add]error 404

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
+  # falseにしたらローカル環境でエラー処理のページを表示させる
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  # エラー404
+  # get '*path', controller: 'application', action: 'render_404'
+
+  # デバイス
   devise_for :users, :controllers => {
     :sessions => 'users/sessions',
     :registrations => 'users/registrations'
@@ -7,6 +11,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   get  'about' => 'homes#about'
 
+  # ユーザー
   scope module: :user do
     resources :users,only: [:show,:edit,:update] do
   		member do
