23d5d2c78fadd0fc2c03020d0ed6b385c3e5ea12
atu <61017515+3flower@users.noreply.github.com>
2020-06-23 17:53:07 +0900

    [Fix]uploader

--- app/uploaders/user_image_id_uploader.rb
+++ app/uploaders/user_image_id_uploader.rb
@@ -5,14 +5,14 @@ class UserImageIdUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  # storage :fog
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+  storage :fog
+  # if Rails.env.development?
+  #   storage :file
+  # elsif Rails.env.test?
+  #   storage :file
+  # else
+  #   storage :fog
+  # end
 
   #サムネイルの為に画像をリサイズ
   version :thumb do
