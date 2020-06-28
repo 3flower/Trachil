e3b325997b58e3ff972a722b79aa2cecdb1edf5b
atu <61017515+3flower@users.noreply.github.com>
2020-06-28 17:31:40 +0900

    [Fix]user-icon

--- app/uploaders/user_image_id_uploader.rb
+++ app/uploaders/user_image_id_uploader.rb
@@ -38,8 +38,8 @@ class UserImageIdUploader < CarrierWave::Uploader::Base
   def default_url(*args)
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-
-    "/images/" + [version_name, "default.png"].compact.join('_')
+    "default.png"
+    # "/images/" + [version_name, "default.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
